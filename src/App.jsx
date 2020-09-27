5e26f89e83b4d563c285b15dc64ec26a5fcb621b
wiebejordan <wiebe.jordan@yahoo.com>
2020-09-26 19:39:53 -0600

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import Landing from '../src/Components/Landing/Landing'
 import routes from './routes';
 import 'semantic-ui-css/semantic.min.css';
-
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
       
       {routes}
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <div style={{marginTop: '30px'}}>
+      <p>Created by Jordan Wiebe 09/2020</p>
+    </div>
+  )
+}
+
+export default Footer
